9ae4378f77fcec35219296309c71da37b0a6f18f
stopiccot <alexey.petruchik@gmail.com>
2014-12-18 21:53:10 +0300

    ololo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
-#include <SDL2/SDL2.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
 
 int main() {
-	return 0;
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+    SDL_Window* sdlWindow = SDL_CreateWindow("tess", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(sdlWindow);
+
+    bool stop = false;
+    while (!stop) {
+        SDL_PumpEvents();
+            
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT: {
+                    stop = true;
+                    break;
+                }
+            }
+        }
+        
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow(sdlWindow);
+    }
+
+    return 0;
 }
